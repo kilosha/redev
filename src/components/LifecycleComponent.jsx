883c307f2deb5622e6d47a808e646796c52ca1f0
Maria Gutkovskaya <gutkovskaya18@gmail.com>
2024-03-19 00:22:07 +0300

    remove unused import

--- src/components/LifecycleComponent.jsx
+++ src/components/LifecycleComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LifecycleComponent = () => {
     const [users, setUsers] = useState([]);
